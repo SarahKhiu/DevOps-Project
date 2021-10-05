4b7cc3a566dd7725d07f748db03a888d8a0a91b1
SarahKhiu <sarahkhiu15@gmail.com>
2021-10-06 02:03:38 +0800

    Inserted script in action js file

--- src/action.js
+++ src/action.js
@@ -2,7 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-  console.log('Hello, world!');
-}
+  const GITHUB_TOKEN = core.getInput ('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
-run();
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! We will review this as soon as we can.'
+  });
+
+run();}
